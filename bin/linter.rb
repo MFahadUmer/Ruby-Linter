29f62bb0bcf99492b519c2e4989e1ff13d5eae0d
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-06-10 03:18:36 +0500

    Add Style Error

---
+++ bin/linter.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require_relative '../lib/file_validation.rb'
+require_relative '../lib/style_validate.rb'
+puts 'Welcome to Linter'
+file_validation = false
+while file_validation == false
+  print 'Please Enter the address of your file: '
+  file = gets.chomp
+  if file.empty?
+    puts 'Empty address! Please Provide a valid address. '
+  else
+    file_validate = FileValidation.new
+    validation = file_validate.file_exist(file)
+    if !validation == true
+      puts 'File not found Or Wrong format of file. PLease select .rb file'
+      puts "Your file address is : #{file}"
+    else
+      puts 'File Loaded'
+      file_validation = true
+    end
+  end
+end
+
+lines = []
+file = File.open(file, 'r')
+while (line = file.gets)
+  lines << line
+end
+validate = StyleError.new
+method_validate = validate.space_between_method_parenthesis(lines)
+method_validate.each { |x| puts x } unless method_validate.empty?
+comma_validate = validate.comma_with_space(lines)
+comma_validate.each { |x| puts x } unless comma_validate.empty?
+indentation_validate = validate.indentation(lines)
+indentation_validate.each { |x| puts x } unless indentation_validate.empty?
+puts 'Great Job! No Error Founds.' if method_validate.empty? && comma_validate.empty? && indentation_validate.empty?
+file.close
---
+++ bin/new.rb
@@ -0,0 +1,11 @@
+my_array = [1, 2, 3, 4, 5, 6, 7]
+class TicTacToe
+end
+if true
+  if true
+    if false
+    end
+  end
+end
+def method()
+end
