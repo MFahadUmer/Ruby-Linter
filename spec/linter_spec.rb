46f84442cb1bad37d2a2f7a6c2416f647bf59548
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-06-10 03:53:11 +0500

    Stickler Error: single quote

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -2,16 +2,16 @@ require './lib/file_validation.rb'
 require './lib/style_validate.rb'
 
 describe FileValidation do
-  describe "#file_exist(file)" do
-    it "Returns true if file exists" do
+  describe '#file_exist(file)' do
+    it 'Returns true if file exists' do
       file = FileValidation.new
-      expect(file.file_exist("./bin/linter.rb")).to eql(true)  
+      expect(file.file_exist('./bin/linter.rb')).to eql(true)  
     end
   end
-  describe "#file_exist(file)" do
-    it "Returns Nil if file does not exists" do
+  describe '#file_exist(file)' do
+    it 'Returns Nil if file does not exists' do
       file = FileValidation.new
-      expect(file.file_exist("./bin/new.rb")).to eql(nil)  
+      expect(file.file_exist('./bin/new.rb')).to eql(nil)  
     end
   end
 end
