22c7a34080261ee41631e19d32fefa66494ed6a7
Muhammad Fahad E Umer <eng.fahadumer@gmail.com>
2020-06-12 01:53:08 +0500

    Stickler Error: Line too long

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Layout/LineLength
 require './lib/file_validation.rb'
 require './lib/style_error.rb'
 
@@ -21,24 +22,24 @@ describe StyleError do
     it 'Returns Empty Array if No space found' do
       validate = StyleError.new
       lines = ['def main()', 'end']
-      expect(validate.space_between_method_parenthesis(lines)).to eql([]) 
+      expect(validate.space_between_method_parenthesis(lines)).to eql([])
     end
     it 'Returns Array with messages if space found' do
       validate = StyleError.new
-      lines = ["def main ()", 'end']
-      expect(validate.space_between_method_parenthesis(lines)).to eql(['Space found before parenthesis. Line No. 1']) 
+      lines = ['def main ()', 'end']
+      expect(validate.space_between_method_parenthesis(lines)).to eql(['Space found before parenthesis. Line No. 1'])
     end
   end
 
   describe 'Indentation' do
     it 'Returns Empty Array if No Indentation Error found' do
       validate = StyleError.new
-      lines = ["class TestClass\n", "  def my_method\n", "    if true\n", "      puts 'Yes'\n", "      puts 'False' if false\n", "      if false\n", "        puts 'Again false'\n", "      end\n", "    end\n", "  end\n", "end"]
-      expect(validate.indentation(lines)).to eql([]) 
+      lines = ["class TestClass\n", "  def my_method\n", "    if true\n", "      puts 'Yes'\n", "      puts 'False' if false\n", "      if false\n", "        puts 'Again false'\n", "      end\n", "    end\n", "  end\n", 'end']
+      expect(validate.indentation(lines)).to eql([])
     end
     it 'Returns Array with messages if Indentations found' do
       validate = StyleError.new
-      lines = ["class TestClass\n", "  def my_method\n", "    if true\n", "      puts 'Yes'\n", "      puts 'False' if false\n", "      if false\n", "          puts 'Again false'\n", "      end\n", "     end\n", "  end\n", "end"]
+      lines = ["class TestClass\n", "  def my_method\n", "    if true\n", "      puts 'Yes'\n", "      puts 'False' if false\n", "      if false\n", "          puts 'Again false'\n", "      end\n", "     end\n", "  end\n", 'end']
       expect(validate.indentation(lines)).to eql(['StyleError:Indentation at Line no. 7  Indentations required: 8', 'StyleError:Indentation at Line no. 9  Indentations required: 4'])
     end
   end
@@ -47,7 +48,7 @@ describe StyleError do
     it 'Returns Empty Array if only one space found after comma' do
       validate = StyleError.new
       lines = ['my_array = [1, 2, 3, 4, 5, 6]']
-      expect(validate.comma_with_space(lines)).to eql([]) 
+      expect(validate.comma_with_space(lines)).to eql([])
     end
     it 'Returns Array with messages if more than one space found after comma' do
       validate = StyleError.new
@@ -60,5 +61,5 @@ describe StyleError do
       expect(validate.comma_with_space(lines)).to eql(['Space After Comma is missing. Line No. 1'])
     end
   end
-  
 end
+# rubocop:enable Layout/LineLength
